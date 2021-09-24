8944401bef6612eeff711f842b620528fef11ddb
Aaiotec <antoniopakas1@gmail.com>
2021-09-24 05:09:14 -0300

    Commit inicial

---
+++ src/main/java/com/antoniopakas/aaiotecstore/AaiotecstoreApplication.java
@@ -0,0 +1,13 @@
+package com.antoniopakas.aaiotecstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AaiotecstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AaiotecstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/antoniopakas/aaiotecstore/AaiotecstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antoniopakas.aaiotecstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AaiotecstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

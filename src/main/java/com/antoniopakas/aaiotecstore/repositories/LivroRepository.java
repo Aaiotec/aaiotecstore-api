e1f34b0932d6407960601a4be8e2e1be673f7a18
Aaiotec <antoniopakas1@gmail.com>
2021-09-25 03:56:29 -0300

    repositories implementados

---
+++ src/main/java/com/antoniopakas/aaiotecstore/repositories/LivroRepository.java
@@ -0,0 +1,12 @@
+package com.antoniopakas.aaiotecstore.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.antoniopakas.aaiotecstore.domain.Livro;
+
+@Repository
+public interface LivroRepository extends JpaRepository<Livro, Integer>{
+
+	
+}
